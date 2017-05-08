2100665038594e3b00f48301722b1cb20b0f75c2
David <davidporter33@icloud.com>
2017-05-08 10:08:48 -0700

    Initial Commit

---
+++ Todo-list/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Todo-list
+//
+//  Created by David Porter on 5/8/17.
+//  Copyright © 2017 David Porter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Todo-list/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Todo-list
+//
+//  Created by David Porter on 5/8/17.
+//  Copyright © 2017 David Porter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
