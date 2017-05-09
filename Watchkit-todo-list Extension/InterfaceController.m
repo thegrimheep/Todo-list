2a046210ee3aaa199f6be1ef92122c92af99e48c
David <davidporter33@icloud.com>
2017-05-09 10:31:31 -0700

    added watchkit interface controller and watch kit extension.

---
+++ Watchkit-todo-list Extension/InterfaceController.m
@@ -0,0 +1,38 @@
+//
+//  InterfaceController.m
+//  Watchkit-todo-list Extension
+//
+//  Created by David Porter on 5/9/17.
+//  Copyright © 2017 David Porter. All rights reserved.
+//
+
+#import "InterfaceController.h"
+
+
+@interface InterfaceController ()
+
+@end
+
+
+@implementation InterfaceController
+
+- (void)awakeWithContext:(id)context {
+    [super awakeWithContext:context];
+
+    // Configure interface objects here.
+}
+
+- (void)willActivate {
+    // This method is called when watch view controller is about to be visible to user
+    [super willActivate];
+}
+
+- (void)didDeactivate {
+    // This method is called when watch view controller is no longer visible
+    [super didDeactivate];
+}
+
+@end
+
+
+
